e5d62a2114196bd9f1c20daee8187d794ee26f67
Lucas Santos <ls6865316@gmail.com>
2023-12-08 11:12:48 -0300

    feat: "Criado Publisher Command Message"

---
+++ src/main/java/com/ead/course/dtos/NotificationCommandDto.java
@@ -0,0 +1,20 @@
+package com.ead.course.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationCommandDto {
+
+    private String title;
+
+    private String message;
+
+    private UUID userId;
+
+}
