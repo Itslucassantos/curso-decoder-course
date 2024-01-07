49cc1610eef6f77f25ed2fb6217fe6fc0fdb0235
Itslucassantos <ls6865316@gmail.com>
2024-01-07 12:22:45 -0300

    feat: "Implementado Basic Authentication no Config Server"

--- src/main/java/com/ead/course/dtos/SubscriptionDto.java
+++ src/main/java/com/ead/course/dtos/SubscriptionDto.java
@@ -1,14 +1,20 @@
 package com.ead.course.dtos;
 
-import lombok.Data;
+
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
-@Data
 public class SubscriptionDto {
 
     @NotNull
     private UUID userId;
 
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
 }
--- src/main/java/com/ead/course/models/UserModel.java
+++ src/main/java/com/ead/course/models/UserModel.java
@@ -11,7 +11,6 @@ import java.io.Serializable;
 import java.util.Set;
 import java.util.UUID;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -46,4 +45,67 @@ public class UserModel implements Serializable {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Set<CourseModel> courses;
 
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Set<CourseModel> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(Set<CourseModel> courses) {
+        this.courses = courses;
+    }
 }
